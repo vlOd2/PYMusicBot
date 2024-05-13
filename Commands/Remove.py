0125e5770c0860ab943bbfde2863b643610c17cc
vlOd <66838724+vlOd2@users.noreply.github.com>
2024-05-13 11:26:05 +0300

    asd

--- Commands/Remove.py
+++ Commands/Remove.py
@@ -3,6 +3,7 @@ import EmbedUtils
 from .Util.CommandUtils import definecmd, guild_user_check, fetch_check, playing_check, channel_check
 from PYMusicBot import PYMusicBot
 from Player.PlayerInstance import PlayerInstance
+from Player.MediaSource import MediaSource
 
 @definecmd("remove", 
            "Removes an item from the queue")
@@ -14,8 +15,21 @@ async def cmd_remove(e : discord.Interaction, index : int):
     if not await playing_check(e, player) or not await channel_check(e, player) or not await fetch_check(e, player):
         return
 
-    await e.response.send_message(embed=EmbedUtils.state(
-        "I'm alive!",
-        "The bot is working perfectly fine!",
-        e.user
+    real_index = index - 1
+
+    if real_index < 0 or real_index >= len(player._queue):
+        await e.response.send_message(embed=EmbedUtils.error(
+            title="Out of range",
+            description="The specified index is outside the queue's range",
+            user=e.user
+        ), ephemeral=True)
+        return
+    
+    source : MediaSource = player._queue.pop(real_index)
+
+    await e.response.send_message(embed=EmbedUtils.success(
+        title="Queue modified",
+        description=(f"Song at index {index} was removed:\n" + 
+                     f"[`{source.title}`]({source.source_url})"),
+        user=e.user
     ))
