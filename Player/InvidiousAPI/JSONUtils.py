f6fe163ba79acc92270629fd09d45dadaa880c20
vlOd <66838724+vlOd2@users.noreply.github.com>
2024-08-04 23:01:42 +0300

    More JSON crap

--- Player/InvidiousAPI/JSONUtils.py
+++ Player/InvidiousAPI/JSONUtils.py
@@ -1,5 +1,5 @@
 import json
-from typing import Any, TypeVar, get_args, get_type_hints
+from typing import TypeVar, get_args, get_type_hints
 
 T = TypeVar("T")
 
@@ -28,28 +28,34 @@ def json_deserialize(data: str | bytes | bytearray, obj: T) -> T:
     if obj.__name__ != "list":
         loaded = json.loads(data)
         if loaded.__class__.__name__ != "dict":
+            if obj.__name__ == "dict":
+                raise ValueError("Data was not a dictionary")
             return loaded
-        
-        if obj.__name__ == "dict":
+        elif obj.__name__ == "dict":
             return loaded
         
         target = obj.__new__(obj)
         target_type_hints = get_type_hints(target)
-        for k, v in loaded.items():
-            if not k in target_type_hints:
-                print(f"{target.__class__.__name__} has no type hint for {k}")
-                target.__dict__[k] = v
+
+        for field, field_type in target_type_hints.items():
+            if field_type.__name__ == "Optional":
+                target.__dict__[field] = None
+
+        for field, value in loaded.items():
+            if not field in target_type_hints:
+                print(f"{target.__class__.__name__} has no type hint for {field}")
+                target.__dict__[field] = value
                 continue
 
-            field_type = target_type_hints[k]
-            target.__dict__[k] = json_deserialize(json_serialize(v), field_type)
+            field_type = target_type_hints[field]
+            target.__dict__[field] = json_deserialize(json_serialize(value), field_type)
 
         return target
     else:
         target = []
         entry_type = get_args(obj)[0]
 
-        for v in json.loads(data):
-            target.append(json_deserialize(json_serialize(v), entry_type))
+        for entry in json.loads(data):
+            target.append(json_deserialize(json_serialize(entry), entry_type))
 
         return target
