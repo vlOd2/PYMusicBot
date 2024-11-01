d77c3dc84ba5a054151c0ce99c48e17ae712b506
vlOd <66838724+vlOd2@users.noreply.github.com>
2024-11-01 21:37:43 +0200

    Bump to 2.0.1

--- Core/Constants.py
+++ Core/Constants.py
@@ -4,7 +4,7 @@ from Updater.Version import Version
 # You can change these if you want, but it's not recommended
 
 # Fundamental
-APP_VERSION = Version(2, 0, 0)
+APP_VERSION = Version(2, 0, 1)
 CONFIG_FILE_NAME = "config.yml"
 LATEST_VERSION_INFO_URL = "https://raw.githubusercontent.com/vlOd2/PYMusicBot/main/LatestVersion.info"
 
